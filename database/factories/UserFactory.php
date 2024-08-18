8cc24391e6c914a5508281023e8ad9fcd670b80e
omgchyah <rossana.liendo@gmail.com>
2024-08-18 14:10:05 +0200

    UserFactory complete

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,9 +23,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+
+        $username = fake()->randomElement([fake()->username()->unique(), 'anonymous']);
+        $email = ($username === 'anonymous') ? fake()->unique()->safeEmail() : $username . fake()->numberBetween(1000, 9999) . "@example.com";
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'username' => $username,
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
