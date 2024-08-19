8b0b27286edc1721277674a9b55a52853240207f
omgchyah <rossana.liendo@gmail.com>
2024-08-19 12:19:30 +0200

    database seeded correctly

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->nullable()->default('anonymous');
+            $table->string('username')->nullable();
             $table->enum('role', ['admin', 'user', 'guest']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
