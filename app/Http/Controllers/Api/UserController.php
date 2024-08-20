08a93c0c94824615f8a48f26458bfe509731b9f9
omgchyah <rossana.liendo@gmail.com>
2024-08-20 12:35:43 +0200

    Created a passport client to use tokens

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,5 +7,25 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    // POST [username, email, password]
+    public function register(Request $request)
+    {
+
+    }
+    //POST [email, password]
+    public function login(Request $request)
+    {
+
+
+    }
+    // GET [Auth: Token]
+    public function profile()
+    {
+
+    }
+    //GET [Auth: Toke]
+    public function logout()
+    {
+        
+    }
 }
