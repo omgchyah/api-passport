75c0b70bf089c9a3413c5733a603e6dba3805fd1
omgchyah <rossana.liendo@gmail.com>
2024-08-23 11:09:34 +0200

    Open routes for login and register, protected routes for profile and logout in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:api');
+})->middleware('auth:api'); */
+
+//Open Routes
+Route::post("register", [UserController::class, "register"]);
+Route::post("login", [UserController::class, "login"]);
+
+//Protected Routes
+Route::group([
+    "midleware" => ["auth:api"]
+], function() {
+    Route::get("profile", [UserController::class, "profile"]);
+    Route::get("logout", [UserController::class, "logout"]);
+});
