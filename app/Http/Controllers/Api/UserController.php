c75d8b17a40e9823d5ce5dc6e1c6d6c91ea05957
omgchyah <rossana.liendo@gmail.com>
2024-08-23 11:49:43 +0200

    Register method working properly

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     // POST [username, email, password]
     public function register(Request $request)
     {
+        //Validation
+        $validatedData = $request->validate([
+            "username" => "nullable|string|min:3",
+            "email" => "required|string|email|unique:users,email",
+            "password" => "required|string|min:8"
+        ]);
 
+    // Create User
+        $user = User::create([
+            "username" => $validatedData["username"] ?? "anonymous", // Default to 'anonymous' if username is null
+            "role" => isset($validatedData["username"]) ? "user" : "guest", // Role is 'user' if username is set, otherwise 'guest'
+            "email" => $validatedData["email"],
+            "password" => bcrypt($validatedData["password"]), // Hash the password
+        ]);
+
+        return response()->json([
+            "user" => $user
+        ], 201);
     }
     //POST [email, password]
     public function login(Request $request)
